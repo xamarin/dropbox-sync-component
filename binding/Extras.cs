aefa15079a80407e548afe23bbf81d7caaffd1f7
Alex Soto <alex@alexsoto.me>
2013-04-04 10:56:02 -0600

    [DropboxSync] Updated Async Methods

--- binding/Extras.cs
+++ binding/Extras.cs
@@ -8,44 +8,79 @@ namespace DropBoxSync.iOS
 	{
 		public Task<DBFileInfo []> ListFolderAsync (DBPath path)
 		{
-			DBError err;
-			return Task<DBFileInfo []>.Factory.StartNew (() => ListFolder (path, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = ListFolder ((DBPath)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, path);
 		}
 
 		public Task<DBFileInfo> FileInfoForPathAsync (DBPath path)
 		{
-			DBError err;
-			return Task<DBFileInfo>.Factory.StartNew (() => FileInfoForPath (path, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = FileInfoForPath ((DBPath)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, path);
 		}
 
 		public Task<DBFile> OpenFileAsync (DBPath path)
 		{
-			DBError err;
-			return Task<DBFile>.Factory.StartNew (() => OpenFile (path, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = OpenFile ((DBPath)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, path);
 		}
 
 		public Task<DBFile> CreateFileAsync (DBPath path)
 		{
-			DBError err;
-			return Task<DBFile>.Factory.StartNew (() => CreateFile (path, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = CreateFile ((DBPath)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, path);
 		}
 
 		public Task<bool> CreateFolderAsync (DBPath path)
 		{
-			DBError err;
-			return Task<bool>.Factory.StartNew (() => CreateFolder (path, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = CreateFolder ((DBPath)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, path);
 		}
 
 		public Task<bool> DeletePathAsync (DBPath path)
 		{
-			DBError err;
-			return Task<bool>.Factory.StartNew (() => DeletePath (path, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = DeletePath ((DBPath)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, path);
 		}
 
 		public Task<bool> MovePathAsync (DBPath fromPath, DBPath toPath)
 		{
-			DBError err;
-			return Task<bool>.Factory.StartNew (() => MovePath (fromPath, toPath, out err)); 
+			return Task<bool>.Factory.StartNew (() => {
+				DBError err;
+				var results = MovePath (fromPath, toPath, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			});
 		}
 	}
 
@@ -53,44 +88,157 @@ namespace DropBoxSync.iOS
 	{
 		public Task<NSFileHandle> ReadHandleAsync ()
 		{
-			DBError err;
-			return Task<NSFileHandle>.Factory.StartNew (() => ReadHandle (out err)); 
+			return Task.Factory.StartNew (()=> {
+				DBError err;
+				var results = ReadHandle (out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			});
 		}
 
 		public Task<NSData> ReadDataAsync ()
 		{
-			DBError err;
-			return Task<NSData>.Factory.StartNew (() => ReadData (out err)); 
+			return Task.Factory.StartNew (()=> {
+				DBError err;
+				var results = ReadData (out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			});
 		}
 
 		public Task<string> ReadStringAsync ()
 		{
-			DBError err;
-			return Task<string>.Factory.StartNew (() => ReadString (out err)); 
+			return Task.Factory.StartNew (()=> {
+				DBError err;
+				var results = ReadString (out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			});
 		}
 
 		public Task<bool> WriteContentsOfFileAsync (string localPath, bool shouldSteal)
 		{
-			DBError err;
-			return Task<bool>.Factory.StartNew (() => WriteContentsOfFile (localPath, shouldSteal, out err)); 
+			return Task.Factory.StartNew (()=> {
+				DBError err;
+				var results = WriteContentsOfFile (localPath, shouldSteal, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			});
 		}
 
 		public Task<bool> WriteDataAsync (NSData data)
 		{
-			DBError err;
-			return Task<bool>.Factory.StartNew (() => WriteData (data, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = WriteData ((NSData)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, data);
 		}
 
 		public Task<bool> WriteStringAsync (string aString)
 		{
-			DBError err;
-			return Task<bool>.Factory.StartNew (() => WriteString (aString, out err)); 
+			return Task.Factory.StartNew (p => {
+				DBError err;
+				var results = WriteString ((string)p, out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}, aString);
 		}
 
 		public Task<bool> UpdateAsync ()
 		{
-			DBError err;
-			return Task<bool>.Factory.StartNew (() => Update (out err)); 
+			return Task.Factory.StartNew (() => {
+				DBError err;
+				var results = Update (out err);
+				if (err != null)
+					throw new DBException (err);
+				return results;
+			}); 
+		}
+	}
+
+	public class DBException : Exception
+	{
+		public DBError NativeError { get; private set; }
+
+		public DBException (DBError err)
+		{
+			NativeError = err;
+		}
+
+		public override string Message {
+			get {
+				switch (NativeError.Code) {
+				case DBErrorCode.CoreSystem:
+					return "System error";
+					
+				case DBErrorCode.Params:
+					return "An error due to data passed into the SDK";
+					
+				case DBErrorCode.ParamsInvalid:
+					return "A parameter is invalid, maybe was null?";
+					
+				case DBErrorCode.ParamsNotFound:
+					return "A file corresponding to a provided path was not found";
+					
+				case DBErrorCode.ParamsExists:
+					return "File already exists and was opened exclusively";
+					
+				case DBErrorCode.ParamsAlreadyOpen:
+					return "File was already open";
+					
+				case DBErrorCode.ParamsParent:
+					return "Parent does not exist or is not a folder";
+					
+				case DBErrorCode.ParamsNotEmpty:
+					return "Directory is not empty";
+					
+				case DBErrorCode.ParamsNotCached:
+					return "File was not yet in cache";
+					
+				case DBErrorCode.System:
+					return "An error in the Dropbox library occurred";
+					
+				case DBErrorCode.SystemDiskSpace:
+					return "An error happened due to insufficient disk space";
+					
+				case DBErrorCode.Network:
+					return "An error occurred making a network request";
+					
+				case DBErrorCode.NetworkTimeout:
+					return "A connection timed out";
+					
+				case DBErrorCode.NetworkNoConnection:
+					return "No network connection available";
+					
+				case DBErrorCode.NetworkSSL:
+					return "Unable to verify the server's SSL certificate. Often caused by an out-of-date clock";
+					
+				case DBErrorCode.NetworkServer:
+					return "Unexpected server error";
+					
+				case DBErrorCode.Auth:
+					return "An authentication related problem occurred";
+					
+				case DBErrorCode.AuthUnlinked:
+					return "The user is no longer linked";
+					
+				case DBErrorCode.AuthInvalidApp:
+					return "An invalid app key or secret was provided";
+					
+				case DBErrorCode.Unknown:
+				default:
+					return "Unknown Error";
+					
+				}
+			}
 		}
 	}
 }
