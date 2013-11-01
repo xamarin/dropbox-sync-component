e034c4446148beb3011071bbfafcfdbd15dcfb91
Zachary Gramana <z.gramana@me.com>
2013-10-31 17:47:40 -0700

    [Android] Finished Android binding. Sample WIP.

--- source/samples/MonkeyBox.Android/MainActivity.cs
+++ source/samples/MonkeyBox.Android/MainActivity.cs
@@ -1,57 +1,101 @@
 using System;
 using Android.App;
 using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
 using DropboxSync.Android;
-using Android.Accounts;
+using System.Linq;
+using Android.App;
+using Android.Views;
+using System.Collections;
+using System.Collections.Generic;
+using MonkeyBox;
 
 namespace MonkeyBox.Android
 {
-  [Activity (Label = "MonkeyBox.Android", MainLauncher = true)]
-  public class MainActivity : Activity
-  {
-    int count = 1;
-    const string DropboxSyncKey = "twjxmah1ytyhlrj"; //"YOUR_APP_KEY";
-    const string DropboxSyncSecret = "be9562vibydmzip"; //"YOUR_APP_SECRET";
-
-    public DBAccountManager Account { get; private set; }
-
-    protected override void OnCreate (Bundle bundle)
+    [Activity (Label = "MonkeyBox.Android", MainLauncher = true)]
+    public class MainActivity : Activity
     {
-      base.OnCreate (bundle);
+        int count = 1;
+        const string DropboxSyncKey = "twjxmah1ytyhlrj";
+        //"YOUR_APP_KEY";
+        const string DropboxSyncSecret = "be9562vibydmzip";
+        //"YOUR_APP_SECRET";
+        public DBAccountManager Account { get; private set; }
 
-      Account = DBAccountManager.GetInstance(ApplicationContext, DropboxSyncKey, DropboxSyncSecret);     
-      Account.LinkedAccountChanged += (sender, e) => Console.WriteLine(e.ToString());
-      if (!Account.HasLinkedAccount)
-      {
-        Account.StartLink(this, (int)RequestCode.LinkToDropboxRequest);
-      }
+        public DBDatastore DropboxDatastore { get; set; }
 
-      // Set our view from the "main" layout resource
-      SetContentView (Resource.Layout.Main);
+        public IEnumerable<Monkey> Monkeys { get; set; }
 
-      // Get our button from the layout resource,
-      // and attach an event to it
-      Button button = FindViewById<Button> (Resource.Id.myButton);
-      
-      button.Click += delegate {
-        button.Text = string.Format ("{0} clicks!", count++);
-      };
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Account = DBAccountManager.GetInstance (ApplicationContext, DropboxSyncKey, DropboxSyncSecret);     
+            Account.LinkedAccountChanged += (sender, e) => Console.WriteLine (e); // TODO: Restart flow.
+
+            if (!Account.HasLinkedAccount) {
+                Account.StartLink (this, (int)RequestCode.LinkToDropboxRequest);
+            } else {
+                InitializeDropbox ();
+                Monkeys = GetMonkeys ();
+                DrawMonkeys (Monkeys);
+            }
+
+        }
+
+        void DrawMonkeys (IEnumerable<Monkey> monkeys)
+        {
+            // Load Monkeys.
+            var nim = new ImageView (this);
+            nim.SetImageResource (Resource.Drawable.Nim);
+            var mainLayout = FindViewById (Resource.Id.main) as RelativeLayout;
+            var layoutParams = new RelativeLayout.LayoutParams (ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+            //            layoutParams.AddRule(LayoutRules.Below)
+            mainLayout.AddView (nim, layoutParams);
+        }
+
+        void InitializeDropbox ()
+        {
+            DropboxDatastore = DBDatastore.OpenDefault (Account.LinkedAccount);
+            DropboxDatastore.DatastoreChanged += (sender, e) =>  {
+                Console.WriteLine ("Datastore needs to be re-synced.");
+                e.P0.Sync ();
+            };
+        }
+
+        IEnumerable<Monkey> GetMonkeys ()
+        {
+            var table = DropboxDatastore.GetTable ("monkeys");
+            var values = new List<Monkey>(6);
+
+            foreach (var row in table.Query ().AsList ()) {
+                values.Add(new Monkey { 
+                    Name = row.GetString("Name"),
+                    Scale = Convert.ToSingle(row.GetString("Scale")),
+                    Rotation = Convert.ToSingle(row.GetString("Rotation")),
+                    X = Convert.ToSingle(row.GetString("X")),
+                    Y = Convert.ToSingle(row.GetString("Y")),
+                    Z = Convert.ToInt32(row.GetString("Z"))
+                });
+            }
+            return values;
+        }
+
+        protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
+        {
+            var code = (RequestCode)requestCode;
+
+            if (code == RequestCode.LinkToDropboxRequest && resultCode != Result.Canceled) {
+                // Start using dropbox.
+                InitializeDropbox ();
+                GetMonkeys ();
+            }
+        }
     }
-
-    protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
-    {
-      var code = (RequestCode)requestCode;
-
-      if (code == RequestCode.LinkToDropboxRequest)
-      {
-        // Start using dropbox.
-      }
-    }
-  }
 }
 
 
--- source/samples/MonkeyBox.Android/Resources/Resource.designer.cs
+++ source/samples/MonkeyBox.Android/Resources/Resource.designer.cs
@@ -45,7 +45,25 @@ namespace MonkeyBox.Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int Fred = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int George = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Hootie = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int Icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int Julian = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int Nim = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int Pepe = 2130837510;
 			
 			static Drawable()
 			{
@@ -61,7 +79,7 @@ namespace MonkeyBox.Android
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int main = 2131034112;
 			
 			static Id()
 			{
